d3c30472a5a2db0bcf00a93c5c5bd08436902e10
unknown <nicolasgonzalez470@gmail.com>
2022-03-02 13:24:56 -0300

    Primera parte finalizada

---
+++ ej1.py
@@ -0,0 +1,49 @@
+import os
+
+
+def carga_keywords():
+	keywords = []
+	try:
+		fichero = open('keywords.txt')
+		print('Archivo cargado correctamente!')
+		keys = fichero.readlines()
+		for palabra in keys:
+			keywords.append(palabra.replace('\n',''))
+	except FileNotFoundError:
+		print('El archivo no existe.')
+	return keywords
+
+def mostrar_keywords(lista):
+	contador = 0
+	for kw in lista:
+		print(kw)
+		contador += 1
+		if contador == 20:
+			contador = 0
+			input('Mostrar mas...')
+	
+def imprimir_menu(menu):
+	for i in range(len(menu)):
+		print(f'[{i}] - {menu[i]}')
+
+n = True
+lista = []
+opciones = ['Salir', 'Importar palabras clave', 'Mostrar palabras clave']
+
+while n:
+	imprimir_menu(opciones)
+	opcion = int(input('>>>'))
+	if opcion == 0:
+		n = False
+	else:
+		if opcion == 1:
+			lista = carga_keywords()
+			
+		else:
+			if len(lista) == 0:
+				print('No descargaste el archivo keywords.txt, intente nuevamente.')
+			else:
+				mostrar_keywords(lista)
+    
+	input('Pulse un boton para continuar...')
+	os.system('cls')
